f42bcc5178b847a3023811eb702f6059c16edf2c
s-evg <s-evg@bk.ru>
2022-07-12 18:25:56 +0500

    В Yndex версии добавлена фильтрация по случайным командам

--- yandex_cloud/bot.py
+++ yandex_cloud/bot.py
@@ -20,18 +20,28 @@ dp = Dispatcher(bot)
 # Удаление сообщений о присоединении новых пользователей
 @dp.message_handler(content_types=["new_chat_members"])
 async def on_user_joined(message: types.Message):
+    print(f"NEW_MEMBERS ===>>> {message}")
     await message.delete()
+
+
+# Удаление случайных комманд /add /show /print
+@dp.message_handler(commands=["add", "show", "print", "start"])
+async def on_user_joined(message: types.Message):
+    print(f"COOMMANDS ===>>> {message.text}")
+    command = message.text.split()
+    if len(command) == 1:
+        await message.delete()
 
 
 async def process_event(event, dp: Dispatcher):
     update = json.loads(event['body'])
+    print(f"UPDATE ===>>> {update}")
     Bot.set_current(dp.bot)
     update = types.Update.to_object(update)
     await dp.process_update(update)
 
 
 async def main(event, context):
+    # print(f"EVENT ===>>> {event}")
     await process_event(event, dp)
-    print(event)
-    print(type(event))
     return {'statusCode': 200, 'body': 'ok'}
